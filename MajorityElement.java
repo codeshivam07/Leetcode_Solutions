ce85db5839a0a1dac0675590cf30e02289468574
codeshivam07 <shivam1149og@gmail.com>
2025-09-19 11:41:02 +0530

    Create MajorityElement.java

---
+++ MajorityElement.java
@@ -0,0 +1,22 @@
+class Solution{
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (count == 0) {
+                max = nums[i];
+                count = 1;
+            } else {
+                if (nums[i] == max) {
+                    count++;
+                } else {
+                    count--;
+                }
+            }
+        }
+
+        return max;
+    }
+}
